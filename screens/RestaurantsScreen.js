fa07d95b72964e8f3e3091f5e2e750967199a809
Ardit <sejeardit@hotmail.com>
2021-10-28 20:55:39 +0200

    added flatlist animation

--- screens/RestaurantsScreen.js
+++ screens/RestaurantsScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import placeholderDataResturants from "../components/browseRestaurants/placeholderDataRestaurants.json";
 import {
   ActivityIndicator,
@@ -14,6 +14,7 @@ import {
   Image,
   RefreshControl,
   TouchableOpacity,
+  Animated,
 } from "react-native";
 
 /*
@@ -24,7 +25,11 @@ import { SearchBar } from "react-native-elements";
 import tw from "tailwind-react-native-classnames";
 import { styleOrangeColor } from "../styles/customStyles";
 import Svg, { Path } from "react-native-svg";
-import {useFocusEffect} from "@react-navigation/native";
+import { useFocusEffect } from "@react-navigation/native";
+
+const SPACING = 1;
+const AVATAR_SIZE = 200;
+const ITEM_SIZE = AVATAR_SIZE + SPACING * 3;
 
 const RestaurantsScreen = ({ navigation }) => {
   const [search, setSearch] = useState("");
@@ -42,14 +47,6 @@ const RestaurantsScreen = ({ navigation }) => {
         console.error(error);
       });
   }, []);*/
-
-  useEffect(() => {
-    return () => {
-      setFilteredDataSource(placeholderDataResturants);
-      setMasterDataSource(placeholderDataResturants);
-      console.log("working fetch useffect");
-    };
-  }, []);
 
   const searchFilterFunction = (text) => {
     // Check if searched text is not blank
@@ -74,50 +71,6 @@ const RestaurantsScreen = ({ navigation }) => {
     }
   };
 
-  const ItemView = ({ item }) => {
-    return (
-      // Flat List Item
-      <TouchableOpacity onPress={() => getItem(item)}>
-        <View style={tw`py-4 px-2 shadow bg-gray-100`}>
-          <View
-            style={tw`flex bg-white rounded-2xl shadow-2xl overflow-hidden`}
-          >
-            <View>
-              <Image
-                style={{ width: "100%", height: 100 }}
-                source={require("../assets/food1.jpg")}
-                resizeMode="cover"
-                resizeMethod="resize"
-              />
-            </View>
-            <View style={tw`flex-row justify-between p-3 items-center`}>
-              <Text
-                style={tw.style(`font-medium`, styleOrangeColor.textOrange)}
-              >
-                {item.title}
-              </Text>
-              <View style={tw`flex-row items-center`}>
-                <Svg
-                  width="18"
-                  height="18"
-                  viewBox="0 0 18 18"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <Path
-                    d="M9 1.5C6.0975 1.5 3.75 3.8475 3.75 6.75C3.75 10.6875 9 16.5 9 16.5C9 16.5 14.25 10.6875 14.25 6.75C14.25 3.8475 11.9025 1.5 9 1.5ZM9 8.625C7.965 8.625 7.125 7.785 7.125 6.75C7.125 5.715 7.965 4.875 9 4.875C10.035 4.875 10.875 5.715 10.875 6.75C10.875 7.785 10.035 8.625 9 8.625Z"
-                    fill="#FF470B"
-                  />
-                </Svg>
-                <Text style={tw.style(`font-medium pl-1`)}>{item.address}</Text>
-              </View>
-            </View>
-          </View>
-        </View>
-      </TouchableOpacity>
-    );
-  };
-
   const ItemSeparatorView = () => {
     return (
       // Flat List Item Separator
@@ -133,6 +86,7 @@ const RestaurantsScreen = ({ navigation }) => {
 
   const getItem = (item) => {
     // Function for click on an item
+    navigation.navigate("QR Scan");
     alert("Id : " + item.id + " Title : " + item.title);
   };
 
@@ -144,13 +98,11 @@ const RestaurantsScreen = ({ navigation }) => {
 
   //focus screen on everytime page is rendered
   useFocusEffect(
-      React.useCallback(() => {
-        setFilteredDataSource(placeholderDataResturants);
-        setMasterDataSource(placeholderDataResturants);
-      }, [])
+    React.useCallback(() => {
+      setFilteredDataSource(placeholderDataResturants);
+      setMasterDataSource(placeholderDataResturants);
+    }, [])
   );
-
-
 
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
@@ -158,6 +110,8 @@ const RestaurantsScreen = ({ navigation }) => {
     setMasterDataSource(placeholderDataResturants);
     wait(2000).then(() => setRefreshing(false));
   }, []);
+
+  const scrollY = useRef(new Animated.Value(0)).current; //remember initial value
 
   return (
     <SafeAreaView style={tw.style(`flex bg-black`)}>
@@ -177,7 +131,12 @@ const RestaurantsScreen = ({ navigation }) => {
           placeholder="Search for restaurant"
           value={search}
         />
-        <FlatList
+        <Animated.FlatList
+          style={tw`bg-gray-100`}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+            { useNativeDriver: true }
+          )}
           refreshControl={
             <RefreshControl
               tintColor="white"
@@ -190,7 +149,99 @@ const RestaurantsScreen = ({ navigation }) => {
           data={filteredDataSource}
           keyExtractor={(item, index) => index.toString()}
           /*ItemSeparatorComponent={ItemSeparatorView}*/
-          renderItem={ItemView}
+          renderItem={({ item, index }) => {
+            //make small animation
+            const inputRange = [
+              -1,
+              0,
+              ITEM_SIZE * index,
+              ITEM_SIZE * (index + 2),
+            ];
+
+            const scale = scrollY.interpolate({
+              inputRange: inputRange,
+              outputRange: [1, 1, 1, 0],
+            });
+
+            //opacity animation
+            const opacityInputRange = [
+              -1,
+              0,
+              ITEM_SIZE * index,
+              ITEM_SIZE * (index + 0.5),
+            ];
+
+            const opacity = scrollY.interpolate({
+              inputRange: opacityInputRange,
+              outputRange: [1, 1, 1, 0],
+            });
+
+            return (
+              <Animated.View
+                style={tw.style(
+                  {
+                    padding: SPACING,
+                    opacity: opacity,
+                    transform: [{ scale }],
+                  },
+                  `bg-gray-100`
+                )}
+                /* style={tw.style(
+                  {
+                    padding: SPACING,
+                    transform: [{ scale }],
+                  },
+                  `bg-gray-100`
+                )}*/
+              >
+                <TouchableOpacity onPress={() => getItem(item)}>
+                  <View style={tw`py-2 px-2 bg-gray-100`}>
+                    <View
+                      style={tw`flex bg-white rounded-2xl shadow-xl overflow-hidden border border-gray-300`}
+                    >
+                      <View>
+                        <Image
+                          style={{ width: "100%", height: 100 }}
+                          source={require("../assets/food1.jpg")}
+                          resizeMode="cover"
+                          resizeMethod="resize"
+                        />
+                      </View>
+                      <View
+                        style={tw`flex-row justify-between p-3 items-center`}
+                      >
+                        <Text
+                          style={tw.style(
+                            `font-medium`,
+                            styleOrangeColor.textOrange
+                          )}
+                        >
+                          {item.title}
+                        </Text>
+                        <View style={tw`flex-row items-center`}>
+                          <Svg
+                            width="18"
+                            height="18"
+                            viewBox="0 0 18 18"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <Path
+                              d="M9 1.5C6.0975 1.5 3.75 3.8475 3.75 6.75C3.75 10.6875 9 16.5 9 16.5C9 16.5 14.25 10.6875 14.25 6.75C14.25 3.8475 11.9025 1.5 9 1.5ZM9 8.625C7.965 8.625 7.125 7.785 7.125 6.75C7.125 5.715 7.965 4.875 9 4.875C10.035 4.875 10.875 5.715 10.875 6.75C10.875 7.785 10.035 8.625 9 8.625Z"
+                              fill="#FF470B"
+                            />
+                          </Svg>
+                          <Text style={tw.style(`font-medium pl-1`)}>
+                            {item.address}
+                          </Text>
+                        </View>
+                      </View>
+                    </View>
+                  </View>
+                </TouchableOpacity>
+              </Animated.View>
+            );
+          }}
         />
       </View>
     </SafeAreaView>
