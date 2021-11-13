0f49d0f8caa40eaffce4e2176ebece4e1dac0bab
Ardit <sejeardit@hotmail.com>
2021-11-13 22:54:00 +0100

    Fixed url problem

--- assets/Url.js
+++ assets/Url.js
@@ -1,9 +1,18 @@
 import React from "react";
+import * as Device from "expo-device";
+
+function isEmulator() {
+  if (Device.osName === "Android") {
+    return "http://10.0.2.2:5000";
+  } else if (Device.osName === "iOS") {
+    return "http://localhost:5000";
+  } else {
+    return "http://localhost:5000";
+  }
+}
 
 export default {
   url: {
-    //url: "http://10.0.2.2:5000", //android emulator
-    url: "http://192.168.1.206:5000", //using phone as emulator check ip (ipconfig) of pc, and insert it.
-    //url: "http://localhost:5000" //ios emulator
+    url: Device.isDevice ? "http://192.168.0.45:5000" : isEmulator(),
   },
 };
